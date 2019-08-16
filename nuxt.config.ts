90d391a63761616e493a5d097294902d937bc6ab
yuhigash <yuma.higashiyama@freegian.co.jp>
2019-08-16 18:46:14 +0900

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import NuxtConfiguration from '@nuxt/config'
+import { Configuration as WebpackConfiguration } from 'webpack'
+// import theme from './config/vuetify'
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const config: NuxtConfiguration = {
+  srcDir: 'src',
+
+  // devModules: ['@nuxtjs/vuetify'],
+  // vuetify: {
+  //   theme,
+  //   customVariables: ['~/assets/css/variables.scss'],
+  //   treeShake: true
+  // },
+
+  build: {
+    extend(config: WebpackConfiguration, { isDev, loaders }) {
+      if (isDev) {
+        config.devtool = 'eval-source-map'
+      }
+      config.resolve = config.resolve || {}
+      config.resolve.plugins = [new TsconfigPathsPlugin({ configFile: './tsconfig.json', extensions: ['.ts', '.vue'] })]
+    }
+  }
+}
+
+export default config
